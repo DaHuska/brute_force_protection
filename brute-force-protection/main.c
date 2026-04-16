64b987f70757c1ea8ab19469916d4a7877042fcb
DaHuska <aboyanov@abv.bg>
2026-04-16 14:22:23 -0400

    Feat: parse users from file

--- brute-force-protection/main.c
+++ brute-force-protection/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "user.c"
+
+int parse_user(const char *line, struct User *user) {
+    return sscanf(line, " %19[^-] - %15s", user->username, user->ip_addr) == 2;
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    // Read users file
+    FILE *fp = fopen("/home/mint/CLionProjects/brute-force-protection/users.txt", "r");
+
+    if (fp == NULL) {
+        perror("File could not be read!");
+        return 1;
+    }
+
+    struct User *users;
+    users = malloc(sizeof(struct User));
+    int count = 0;
+    char buff[256];
+
+    while (fgets(buff, sizeof(buff), fp) != NULL) {
+        buff[strcspn(buff, "\n")] = '\0';
+
+        if (parse_user(buff, &users[count])) {
+            count++;
+
+            struct User *temp_users = realloc(users, sizeof(struct User) * count);
+            users = temp_users;
+        }
+    }
+
+    fclose(fp);
+
+    for (int i = 0; i < count; i++) {
+        printf("User %s - %s\n", users[i].username, users[i].ip_addr);
+    }
+
+    free(users);
     return 0;
 }
---
+++ brute-force-protection/user.c
@@ -0,0 +1,5 @@
+struct User {
+    char username[20];
+    char role[5];
+    char ip_addr[16];
+};
