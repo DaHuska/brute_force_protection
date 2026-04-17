3e2d4c7e3d4c21c1589610c3528710d952005599
DaHuska <aboyanov@abv.bg>
2026-04-17 11:29:20 -0400

    Feat: user login

--- brute-force-protection/main.c
+++ brute-force-protection/main.c
@@ -4,7 +4,34 @@
 
 #include "user.c"
 
-// TODO: second user IP not  saved
+int validateCreds(struct User *users, int size, char *username, char *password) {
+    for (int i = 0; i < size; ++i) {
+        if (strcmp(users[i].username, username) == 0 &&
+            strcmp(users[i].password, password) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void login(struct User *users, int size) {
+    char ip_addr[16];
+    char username[20];
+    char password[20];
+
+    scanf(" %15[^-] - %19[^:]:%19s", ip_addr, username, password);
+
+    // Validate credentials
+    if (validateCreds(users, size, username, password)) {
+        printf("Login successful!\n");
+    } else {
+        printf("Failed attempt!\n");
+    }
+
+    printf("%s %s %s\n", ip_addr, username, password);
+}
+
 int parse_user(const char *line, struct User *user) {
     return sscanf(line, " %15[^-] - %19[^:]:%19s", user->ip_addr, user->username, user->password) == 3;
 }
@@ -54,7 +81,9 @@ int main(void) {
 
     fclose(fp);
 
-    for (int i = 0; i < count; i++) {
+    login(users, count);
+
+    for (int i = 0; i < count - 1; i++) {
         printf("User %s - %s\n", users[i].username, users[i].ip_addr);
     }
 
